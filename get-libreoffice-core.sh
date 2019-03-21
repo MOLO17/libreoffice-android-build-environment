4d2f48ee4e600d36b66ff9301e2437e2abdda48c
Michelangelo Altamore <michelangelo.altamore@molo17.com>
2019-03-21 18:39:16 +0100

    Fix libreoffice script

--- get-libreoffice-core.sh
+++ get-libreoffice-core.sh
@@ -6,7 +6,7 @@ LO_CORE=${LO_CORE:=libreoffice-core}
 
 git clone https://github.com/MOLO17/${LO_CORE} --depth 100 \
 && cp autogen.input.example ${LO_CORE}/autogen.input \
-&& cp LibreOfficeAndroidCustom.conf ${LO_CORE}/distro-configs/
-&& cd ${LO_CORE}
-&& ./autogen.sh
+&& cp LibreOfficeAndroidCustom.conf ${LO_CORE}/distro-configs/ \
+&& cd ${LO_CORE} \
+&& ./autogen.sh \
 && true
